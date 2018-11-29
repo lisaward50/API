a04e02c2f9c3f251b35397d469667f7b04f5bf7a
lisaward50 <lisa.ward50@gmail.com>
2018-11-29 11:22:19 -0500

    Updates

---
+++ precache-manifest.fe66970b7962d70daeb2b048009b63e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c583796848975bf9d66d",
+    "url": "/API/static/css/main.b53b9a56.chunk.css"
+  },
+  {
+    "revision": "c583796848975bf9d66d",
+    "url": "/API/static/js/main.c5837968.chunk.js"
+  },
+  {
+    "revision": "4aef88a34e44043a546f",
+    "url": "/API/static/js/1.4aef88a3.chunk.js"
+  },
+  {
+    "revision": "04aa36bb15e928bf4348",
+    "url": "/API/static/js/runtime~main.04aa36bb.js"
+  },
+  {
+    "revision": "df4829eb3b394157b2743166ba69accf",
+    "url": "/API/index.html"
+  }
+];
